a3bdd8ee1c78a32933eb2b16e539841c284932a9
Mukesh Karn <mukeshkarn@gmail.com>
2015-12-13 10:59:55 +0545

    Testing with dummy data

---
+++ README.md
@@ -0,0 +1,5 @@
+Sector
+=======
+
+
+Development Repo for the sectoral dashboard
---
+++ js/year.js
@@ -0,0 +1,8 @@
+  
+
+function myYear(sel)
+{
+var value = sel.value
+var url = "index.html?Yr="+value
+window.location.href = url;
+}		  
